521426e5f0f4eb9271d1b48d193176c9c99e3de5
Larkin (Work) <larkindunlop2@gmail.com>
2026-02-26 15:22:40 +1100

    receiving messages properly

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-
 import (
 	"context"
 	"fmt"
@@ -8,18 +7,98 @@ import (
 	"os/signal"
 	"syscall"
 
-	"go.mau.fi/whatsmeow"
-	"go.mau.fi/whatsmeow/store/sqlstore"
-	"go.mau.fi/whatsmeow/types/events"
-	waLog "go.mau.fi/whatsmeow/util/log"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/mdp/qrterminal"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types"
+	watypes "go.mau.fi/whatsmeow/types"
+	"go.mau.fi/whatsmeow/types/events"
+	waLog "go.mau.fi/whatsmeow/util/log"
 )
 
-func eventHandler(evt interface{}) {
+// client struct to pass the client to the event handler
+type MyClient struct {
+	WAClient *whatsmeow.Client
+	eventHandlerID uint32
+}
+
+// register helper
+func (myclient *MyClient) register() {
+	myclient.eventHandlerID = myclient.WAClient.AddEventHandler(myclient.myEventHandler)
+}
+
+// main event handler function
+func (myclient *MyClient) myEventHandler(evt any) {
 	switch v := evt.(type) {
 	case *events.Message:
-		fmt.Println("Received a message!", v.Message.GetConversation())
+		// initial vars 
+		ctx := context.Background()
+		client := myclient.WAClient
+		sender := v.Info.Sender
+		chat := v.Info.Chat 
+
+		// If this is a broadcast message, skip
+		if chat.Server == watypes.BroadcastServer {
+			fmt.Println("New status update!")
+			fmt.Println()
+			return
+		}
+
+		// get the phone number of the user because it sometimes passes a jid or lid not a phone number.
+		var phoneNumber string
+		if sender.Server == watypes.HiddenUserServer {
+			pnJID, err := client.Store.LIDs.GetPNForLID(ctx, sender)
+			if err == nil && !pnJID.IsEmpty() {
+				phoneNumber = pnJID.User
+			} else {
+				phoneNumber = sender.User
+			}
+		} else {
+			phoneNumber = sender.User
+		}
+
+		// get the jid from the phone number (i know it seems backward but it's becasue of lids)
+		jid := watypes.NewJID(phoneNumber, types.DefaultUserServer)
+
+		// obtain the contact from the jid
+		contact, err := myclient.WAClient.Store.Contacts.GetContact(ctx, jid)
+
+		// get the contact's saved name with backups.
+		var savedName string
+		if err == nil && contact.Found {
+			savedName = contact.FullName
+			if savedName == "" {
+				savedName = contact.FirstName
+			}
+			if savedName == "" {
+				savedName = contact.PushName
+			}
+		} else {
+			savedName = "Error fetching name"
+		}
+
+		// get the message text
+		text := v.Message.GetConversation()
+		if text == "" {
+			text = v.Message.GetExtendedTextMessage().GetText()
+		}
+
+		fmt.Println("New Message:", text)
+		if sender == chat {
+			fmt.Println("From senderPN:", phoneNumber)
+		} else {
+			fmt.Println("From senderPN:", phoneNumber, "On chat:", chat)
+		}
+		fmt.Println("Sender name:", savedName) 
+		fmt.Println()
+		/*
+		if phoneNumber == "61486036614" {
+			client := myclient.WAClient
+			reaction := client.BuildReaction(v.Info.Chat, v.Info.Sender, v.Info.ID, "🐈‍⬛")
+			myclient.WAClient.SendMessage(context.Background(), v.Info.Chat, reaction)
+		}
+		*/
 	}
 }
 
@@ -37,9 +116,11 @@ func main() {
 		panic(err)
 
 	}
-	clientLog := waLog.Stdout("Client", "DEBUG", true)
-	client := whatsmeow.NewClient(deviceStore, clientLog)
-	client.AddEventHandler(eventHandler)
+	// clientLog := waLog.Stdout("Client", "DEBUG", true)
+	tempcli := whatsmeow.NewClient(deviceStore, nil)
+	clientstruct := MyClient { tempcli, 0 }
+	clientstruct.register()
+	client := clientstruct.WAClient
 
 	if client.Store.ID == nil {
 
@@ -63,7 +144,6 @@ func main() {
 		// Already logged in, just connect
 		err = client.Connect()
 		if err != nil {
-
 			panic(err)
 		}
 	}
